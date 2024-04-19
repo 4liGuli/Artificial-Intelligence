d12351e70de164376c7a7bc09522a3722ed75985
4liGuli <AliGuliyev2005@gmail.com>
2024-04-20 02:37:37 +0400

    tab

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ const informationsizess = document.querySelector(".information .about");
 const menuBtn = document.querySelector(".fa-bars");
 const hidemenuBtn = informationsizess.querySelector(".fa-xmark");
 const showPopupBtn = document.querySelector(".signin-btn");
+const showVideoo = document.querySelector(".watchvideo");
 const formPopun = document.querySelector(".form-popup");
 const hidePopupBtn = document.querySelector(".form-popup .fa-xmark");
 const loginSignupLink = document.querySelectorAll(".from-box .bottom-link a");
@@ -14,6 +15,10 @@ hidemenuBtn.addEventListener("click", () => menuBtn.click());
 
 showPopupBtn.addEventListener("click", () =>{
     document.body.classList.toggle("show-popup");
+});
+
+showVideoo.addEventListener("click", () =>{
+    document.body.classList.toggle("show-video");
 });
 
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
